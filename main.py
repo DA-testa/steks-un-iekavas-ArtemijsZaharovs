e2f2c578e04d19b04b3b3902e1c8ea0dc74120e0
ArtemijsZaharovs <124812244+ArtemijsZaharovs@users.noreply.github.com>
2023-02-19 17:53:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 //221RDC035, Artemijs Zaharovs, 18.gr.
 from collections import namedtuple
 
-
 Bracket = namedtuple("Bracket", ["char", "position"])
 
 
@@ -10,34 +9,34 @@ def are_matching(left, right):
 
 
 def find_mismatch(text):
-    stack = []
-    for i, char in enumerate(text):
-        if char in "([{":
-            stack.append(Bracket(char, i))
-        elif char in ")]}":
-            if not stack or not are_matching(stack[-1].char, char):
-                return i
-            stack.pop()
-    return stack[-1].position if stack else None
+    opening_brackets_stack = []
+    for i, next_char in enumerate(text):
+        if next_char in "([{":
+            opening_brackets_stack.append(Bracket(next_char, i))
 
+        if next_char in ")]}":
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1][0], next_char):
+                return i + 1
+            else:
+                opening_brackets_stack.pop()
+
+    if opening_brackets_stack:
+        return opening_brackets_stack[-1][1] + 1
 
 def main():
-    input_type = input("Enter input type (I for user input, F for file input): ")
-    if input_type == "I":
-        text = input("Enter text: ")
-    elif input_type == "F":
-        filename = input("Enter filename: ")
-        with open(filename, "r") as f:
+    text = input().strip()
+    if text == 'I':
+        text = input().strip()
+    elif text == 'F':
+        file_path = input().strip()
+        with open(file_path) as f:
             text = f.read()
-    else:
-        print("Invalid input type.")
-        return
 
-    mismatch_pos = find_mismatch(text)
-    if mismatch_pos is None:
+    mismatch = find_mismatch(text)
+    if not mismatch:
         print("Success")
     else:
-        print(f"Mismatch at position {mismatch_pos}")
+        print(mismatch)
 
 
 if __name__ == "__main__":
