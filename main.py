ee90db6114cd564eadb13d9945c19a8d87e4866e
ArtemijsZaharovs <124812244+ArtemijsZaharovs@users.noreply.github.com>
2023-02-19 18:11:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,39 @@
 #221RDC035, Artemijs Zaharovs, 18.gr.
-
 from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
 
 def are_matching(left, right):
-    
     return (left + right) in ["()", "[]", "{}"]
 
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            # Process opening bracket, write your code here
-            opening_brackets_stack.append(Bracket(next, i))
-            #Bracket.char = next
-            #Bracket.position = i
-            pass
-            
+    for i, next_char in enumerate(text):
+        if next_char in "([{":
+            opening_brackets_stack.append(Bracket(next_char, i))
 
-        if next in ")]}":
-           
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[len(opening_brackets_stack)-1][0], next):
-                #print(i+1)
-                return i + 1 
-                
-            if are_matching(opening_brackets_stack[len(opening_brackets_stack)-1][0], next):
+        if next_char in ")]}":
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1][0], next_char):
+                return i + 1
+            else:
                 opening_brackets_stack.pop()
-           
-            pass
-    
-    if opening_brackets_stack :
-        return opening_brackets_stack[len(opening_brackets_stack)-1][1] + 1
+
+    if opening_brackets_stack:
+        return opening_brackets_stack[-1][1] + 1
 
 def main():
-    text = input()
-    if 'I' in text:
-        text = input()
-    elif 'F' in text:
-        file = "./test/5"
-        with open(file) as f:
+    text = input().strip()
+    if text == 'I':
+        text = input().strip()
+    elif text == 'F':
+        file_path = input().strip()
+        with open(file_path) as f:
             text = f.read()
 
     mismatch = find_mismatch(text)
-   
-    if not mismatch :
+    if not mismatch:
         print("Success")
     else:
         print(mismatch)
